6154a2c67fdf2d314aa3be0be4ebfe04e8189e92
Leandro Alexandre <leandroalexanndre719@gmail.com>
2025-08-15 11:42:17 -0300

    novo exercicio

---
+++ ex021.1.py
@@ -0,0 +1,2 @@
+from playsound3 import playsound
+playsound("https://music.youtube.com/watch?v=VVLAnjtpXeM&si=1w7eUTipe6d9E1wR")
--- ex021.py
+++ ex021.py
@@ -1,6 +1,2 @@
-import pygame
-pygame.init()
-pygame.mixer.music.load("ex021.mp3")
-pygame.mixer.music.play()
-pygame.event.wait()
-
+from playsound3 import playsound
+playsound("ex021.mp3")
